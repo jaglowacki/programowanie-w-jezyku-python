06ef5a75428feaaa0c01c871927f0bf248235e4b
jglowacki@int.pl <you@example.com>
2025-11-25 13:04:48 +0100

    Zadanie 2 poprawki

--- zad_2.py
+++ zad_2.py
@@ -1,12 +1,12 @@
-def pomnoz2A(lista):
+def multiply2a(lista):
     nowa_lista=[]
     for el in lista:
         nowa_lista.append(el*2)
     return nowa_lista
 
-def pomnoz2B(lista):
+def multiply2b(lista):
     nowa_lista=[el*2 for el in lista]
     return nowa_lista
 
-print(pomnoz2A([11,22,33,44,55]))
-print(pomnoz2B([11,22,33,44,55]))
+print(multiply2a([11,22,33,44,55]))
+print(multiply2b([11,22,33,44,55]))
