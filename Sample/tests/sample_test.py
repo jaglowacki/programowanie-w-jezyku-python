ce97a8ea735f5fd74104501ec16eae0c73b6e888
Janusz Glowacki <jglowacki@int.pl>
2025-12-15 12:20:09 +0100

    trying test with to attemps

--- Sample/tests/sample_test.py
+++ Sample/tests/sample_test.py
@@ -1,4 +1,8 @@
 import Sample.sample as sample
 
-def test_answer():
-    assert sample.func(3) == 11
+def test_answer_1():
+    assert sample.func(3) == 3
+    assert sample.func(3) == 5
+
+def test_answer_2():
+    assert sample.func(3) == 5
